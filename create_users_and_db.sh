f021fe3db908313069213054564dd2092b4cf321
Lemetist <lemetist@gmail.com>
2025-03-20 00:19:20 +0300

    last fix maybe

--- create_users_and_db.sh
+++ create_users_and_db.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# Функция для генерации случайного пароля
+# генерация пароля
 generate_password() {
   tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6
 }
 
-# Создаем 11 пользователей, 11 баз данных и таблицу для хранения данных
+# 11 пользователей и баз данных
 for i in $(seq 1 11); do
-  # Создаем пароль для каждого пользователя
+  # переменные
   password=$(generate_password)
+  root_password="011220"
 
   # Создаем пользователя и базу данных
-  mysql -e "CREATE DATABASE DB$i;"
-  mysql -e "CREATE USER 'u$i'@'localhost' IDENTIFIED BY '$password';"
-  mysql -e "GRANT ALL PRIVILEGES ON DB$i.* TO 'u$i'@'localhost';"
-  mysql -e "REVOKE CREATE, DROP ON DB$i.* FROM 'u$i'@'localhost';"
-  mysql -e "FLUSH PRIVILEGES;"
+  mysql -u root -p$root_password -e "CREATE DATABASE DB$i;"
+  mysql -u root -p$root_password -e "CREATE USER 'u$i'@'localhost' IDENTIFIED BY '$password';"
+  mysql -u root -p$root_password -e "GRANT ALL PRIVILEGES ON DB$i.* TO 'u$i'@'localhost';"
+  mysql -u root -p$root_password -e "REVOKE CREATE, DROP ON DB$i.* FROM 'u$i'@'localhost';"
+  mysql -u root -p$root_password -e "FLUSH PRIVILEGES;"
 
-  # Заполним таблицу Users (создадим ее если не существует)
+  # Заполним таблицу Users в базе данных DB
   if [ $i -eq 1 ]; then
-    mysql -e "CREATE DATABASE DB;"
-    mysql -e "CREATE TABLE DB.Users (username VARCHAR(50), password TEXT);"
+    mysql -u root -p$root_password -e "CREATE DATABASE DB;"
+    mysql -u root -p$root_password -e "CREATE TABLE DB.Users (username VARCHAR(50), password TEXT);"
   fi
 
-  # Вставим пользователя и его зашифрованный пароль
+  # Вставим пользователя и пароль в таблицу Users
   encrypted_password=$(echo -n "$password" | sha256sum | awk '{print $1}')
-  mysql -e "INSERT INTO DB.Users (username, password) VALUES ('u$i', '$encrypted_password');"
+  mysql -u root -proot_password -e "INSERT INTO DB.Users (username, password) VALUES ('u$i', '$encrypted_password');"
 done
 
 echo "Пользователи и базы данных созданы успешно!"
-
